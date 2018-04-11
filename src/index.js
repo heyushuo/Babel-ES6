6bf20062b554e7f504495af2c416f053c16a33de
heyushuo <1007561607@qq.com>
2018-04-11 16:52:02 +0800

    finish

---
+++ src/index.js
@@ -0,0 +1,16 @@
+var arr=['a','b','c'];
+
+var newArr=arr.map((item,index)=>{
+	item +'1'
+})
+const name='heyushuo'
+var str=`
+		<div>
+			<h1>的饭卡时代峻峰</h1>
+			<ul>
+				<li>
+					士大夫撒旦法
+				</li>
+			</ul>
+		</div>
+	`
